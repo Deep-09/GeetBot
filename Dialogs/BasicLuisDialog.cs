b5d97e880276315259d3af93fe726104d1eccb26
Deep-09 <deepbhasme@gmail.com>
2018-01-31 16:07:11 +0530

    Initial test

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -5,50 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Cognitive.LUIS.ActionBinding.Bot;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
     // For more information about this template visit http://aka.ms/azurebots-csharp-luis
     [Serializable]
-    public class BasicLuisDialog : LuisDialog<object>
+    public class BasicLuisDialog : LuisActionDialog<object>
     {
-        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
+        public BasicLuisDialog() : base(
+            new Assembly[] { typeof(SoftwareInstallAction).Assembly },
+            (action, context) =>
+            {
+            },
+            new LuisService(new LuisModelAttribute(
             ConfigurationManager.AppSettings["LuisAppId"], 
             ConfigurationManager.AppSettings["LuisAPIKey"], 
             domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
         {
         }
 
-        [LuisIntent("None")]
-        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        [LuisIntent("Software Installation")]
+        public async Task IntentActionResultHandlerAsync(IDialogContext context, object actionResult)
         {
-            await this.ShowLuisResult(context, result);
-        }
+            // we know these actions return a string for their related intents,
+            // although you could have individual handlers for each intent
+            var message = context.MakeMessage();
 
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "Gretting" with the name of your newly created intent in the following handler
-        [LuisIntent("Greeting")]
-        public async Task GreetingIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
+            message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
 
-        [LuisIntent("Cancel")]
-        public async Task CancelIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
-        [LuisIntent("Help")]
-        public async Task HelpIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
-        private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
-        {
-            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
-            context.Wait(MessageReceived);
+            await context.PostAsync(message);
         }
     }
 }
